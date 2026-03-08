efb63535286fe26ff52668d74d59eb9ddace5e62
MarkBish24 <markbish24@gmail.com>
2026-03-08 14:27:56 -0700

    positioning of house is good

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,22 @@ import Loader from "../components/Loader";
 import Island from "../models/Island";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let screenRotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, screenRotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
   return (
     <section className="w-full h-screen relative">
       <Canvas
@@ -22,7 +38,11 @@ const Home = () => {
           <spotLight />
           <hemisphereLight />
 
-          <Island />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
