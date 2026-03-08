372d2bd124730620020f395640285c060cc53055
MarkBish24 <markbish24@gmail.com>
2026-03-08 12:57:30 -0700

    routers created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
-        NavBar
+        <NavBar />
         <Routes>
           <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/contact" element={"Contact"} />
+          <Route path="/projects" element={"Projects"} />
         </Routes>
       </Router>
     </main>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">MB</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/about"
+          className={(isActive) =>
+            isActive ? "text-blue-500" : "text-gray-500"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
