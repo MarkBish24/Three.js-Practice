d30e15959823d9c95e8a83dcf4112d04e3cd7638
MarkBish24 <markbish24@gmail.com>
2026-03-08 12:47:32 -0700

    Tailwind and router instal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    )
-}
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        NavBar
+        <Routes>
+          <Route path="/" element={"Home"} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import ReactDOM from "react-dom/client"
+import React from "react"
 import App from "./app"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
